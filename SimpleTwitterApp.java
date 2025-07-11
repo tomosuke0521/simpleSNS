652df4ccf3a5a9b4ebe905dd117f9a8fa6a58c16
tomosuke0521 <ogawanaho5@gmail.com>
2025-07-11 16:04:56 +0900

    #finish

--- SimpleTwitterApp.java
+++ SimpleTwitterApp.java
@@ -28,6 +28,7 @@ public class SimpleTwitterApp{
                 String tweet = tweetInput.getText();
                 if(!tweet.isEmpty()){
                     addTweet(tweet);
+                    tweetInput.setText("");
                 }
             }
         });
@@ -35,7 +36,8 @@ public class SimpleTwitterApp{
     }
 
     private void addTweet(String tweet){
-        tweetDisplay.append(tweet + "\n\n");
+        LocalDate timestamp = LocalDate.now();
+        tweetDisplay.append(timestamp + ":" + tweet + "\n\n");
     }
 
     public static void main(String[] args){
