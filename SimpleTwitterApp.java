30dc620149c98b0cdfbabc45b1dec1fe2aee7c2d
tomosuke0521 <ogawanaho5@gmail.com>
2025-07-11 15:43:50 +0900

    #made userform

--- SimpleTwitterApp.java
+++ SimpleTwitterApp.java
@@ -1,10 +1,21 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class SimpleTwitterApp{
     JFrame frame = new JFrame();
+    private JTextField tweetInput = new JTextField();
+    private JButton tweetButton = new JButton();
+
     public SimpleTwitterApp(){
         frame.setTitle("シンプルなSNS");
         frame.setSize(400,300);
+
+        JPanel inputPanel = new JPanel(new BorderLayout());
+
+        inputPanel.add(tweetInput,BorderLayout.CENTER);
+        inputPanel.add(tweetButton,BorderLayout.EAST);
+
+        frame.add(inputPanel,BorderLayout.NORTH);
     }
 
     public static void main(String[] args){
